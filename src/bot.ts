0a8caf32f1444273194a34e56bff745d60b1cd61
KingOKarma <56193275+KingOKarma@users.noreply.github.com>
2021-08-03 00:18:45 +0100

    Initial commit

---
+++ src/bot.ts
@@ -0,0 +1,4 @@
+import ChatClient, { authProvider } from "./client/client";
+import { CONFIG } from "./utils/globals";
+
+new ChatClient(authProvider, { channels: [CONFIG.twitchUsername] }).initChatClient().catch(console.error);
